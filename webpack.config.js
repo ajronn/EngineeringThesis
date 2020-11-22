d071192a797422b53d350228899e0eb2ea761604
AjronN <34678770+ajronn@users.noreply.github.com>
2020-11-22 18:16:42 +0100

    fix config

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,41 @@ module.exports = {
     entry: "./src/index.tsx",
     output: {
         path: path.resolve(__dirname, "build"),
-        filename: "bundle.js"
+        filename: "bundle.js",
+        publicPath: "/"
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                loader: "awesome-typescript-loader"
+                loader: 'awesome-typescript-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: path.resolve(__dirname, 'src/styles/'),
+                use: [
+                    'style-loader',
+                    'css-modules-typescript-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]___[hash:base64:5]',
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.scss$/,
+                include: path.resolve(__dirname, 'src/styles/'),
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             }
         ]
+    },
+    devServer: {
+        historyApiFallback: true,
     },
     resolve: {
         modules: ["src", "node_modules"],
